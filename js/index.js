a4d2f2c7e10c9c1ebe72f3d968b541a30c899609
AlanAcostaMx <70784421+AlanAcostaMx@users.noreply.github.com>
2023-10-12 20:36:24 -0600

    Omision de comentarios
    Se borran comentarios y saltos de parrafo para minimizar el tamaño del archivo

--- js/index.js
+++ js/index.js
@@ -7,36 +7,30 @@ $(document).ready(function(){
     var elem_idStr = '';
 
     if(input == ''){
-      // console.log("Input vacio! Mostrando botones...");
       $('.titulo-serv').hide();
       $('.servicios').hide();
       $('.showHide').show();
-    }else{
-      // Este ciclo recorre todas las ULs con la clase "servicios", las ULs son las que tienen los datos de contacto de cada servicio  
+    }else{  
       for(let i = 0; i < services.length; i++){
-          elem_id = $(services[i]).attr('id').toLowerCase(); // ul-albanileria
-          elem_idStr = elem_id.substring(3); // albanileria
+          elem_id = $(services[i]).attr('id').toLowerCase();
+          elem_idStr = elem_id.substring(3);
           let ulId = '';
           ulId = $('#' + elem_id);
           let titId = '';
           titId = $('#tit-' + elem_idStr);
           
           if (!elem_idStr.includes(input)) {
-            // Mostrar todos los botones y ocultar todas las ULs
             titId.hide();
             ulId.hide();
           } else {
-            // Mostrar ul y ocultar el resto de BTNs y ULs
             $('.showHide').hide();
 
             if(ulId.show()){
-              // console.log("Elemento anteriormente mostrado! " + elem_idStr);
               titId.show();
             }else{
               $('.servicios').hide();
               titId.hide();
             }
-            // titId.show();
             ulId.show();
           }
       }
